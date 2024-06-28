7763c2be3f299e657c369492a87ef1670fb70d4a
om7057 <kulkarniom7057@gmail.com>
2024-06-29 01:11:58 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -9,15 +9,29 @@ const port = process.env.PORT || 5000; // Set a default port if not defined in .
 // Connect to MongoDB
 connectToMongo();
 
-// Middleware
-app.use(cors());
+// CORS configuration
+const allowedOrigins = ['http://localhost:5173', 'https://redhouse-edtech.netlify.app'];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true // If you want to allow cookies or HTTP authentication
+}));
+
 app.use(express.json());
 
 // Routes
 app.use("/api/auth", require("./src/routes/authRoutes"));
 app.use("/api/subjects", require("./src/routes/subjectRoutes"));
 app.use("/api/quizzes", require("./src/routes/quizRoutes"));
-app.use('/api/contactUs',require("./src/routes/contactRoutes"));
+app.use('/api/contactUs', require("./src/routes/contactRoutes"));
 
 // Start the server
 app.listen(port, () => {
